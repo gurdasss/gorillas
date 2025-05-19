e083a07e9d40895f7a7a2c769aeb008b3606361b
Gurdas Satwani <gurdassatwani1@gmail.com>
2025-05-19 18:51:58 +0530

    worked a little!

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
+#include "Rectangle2D.h"
+#include "Random.h"
 #include <raylib.h>
+#include <forward_list>
+
+void initBuildings(std::forward_list<Rectangle2D> &buildings);
 
 int main()
 {
@@ -9,10 +14,18 @@ int main()
 
     SetTargetFPS(60);
 
+    // a list to store all the buildings
+    std::forward_list<Rectangle2D> buildings{};
+
+    initBuildings(buildings);
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
+
+        for (const Rectangle2D &building : buildings)
+            DrawRectangleRec(building.getRectangle(), building.getTint());
 
         DrawFPS(0, 0);
 
@@ -22,4 +35,13 @@ int main()
     CloseWindow();
 
     return 0;
+}
+
+void initBuildings(std::forward_list<Rectangle2D> &buildings)
+{
+    const float maxScreenH{static_cast<float>(GetScreenHeight()) / 2.0f};
+    const float minScreenH{maxScreenH / 2.0f};
+
+    constexpr float maxBuildingW{50.0f};
+    constexpr float minBuildingW{20.0f};
 }
