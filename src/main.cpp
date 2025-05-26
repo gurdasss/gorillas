9e80449f84ba40476f62f977bb96c98a8dad6102
Gurdas Satwani <gurdassatwani1@gmail.com>
2025-05-26 14:02:50 +0530

    added gravity to the banana

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include "Rectangle2D.h"
+#include "Circle2D.h"
+#include "Line2D.h"
 #include "Random.h"
 #include <raylib.h>
+#include <raymath.h>
 #include <forward_list> // for std::forward_list
 #include <iostream>
 #include <cassert> // for assert
+#include <cmath>   // for std::sin & std::cos
 
 void initBuildings(std::forward_list<Rectangle2D> &buildings);
 void placeGorillaOnBuilding(const std::forward_list<Rectangle2D> &buildings,
@@ -17,7 +21,8 @@ int main()
 
     InitWindow(screenW, screenH, "Gorillas");
 
-    SetTargetFPS(60);
+    constexpr int targetFPS{60};
+    SetTargetFPS(targetFPS);
 
     // a list to store all the buildings
     std::forward_list<Rectangle2D> buildings{};
@@ -42,8 +47,89 @@ int main()
                            screenW - screenW / 4,
                            screenW);
 
+    Circle2D banana{};
+    banana.setRadius(20);
+    banana.setTint(RED);
+    banana.setPosition(lGorilla.getPosition());
+
+    Vector2 velocity{};
+    Vector2 acceleration{};
+
+    Line2D arrow{};
+    arrow.setStartPos(lGorilla.getPosition());
+    arrow.setTint(RED);
+
+    bool toggleChance{};
+
     while (!WindowShouldClose())
     {
+        /*
+        1. apply acceleration to velocity
+        2. apply velocity to object's position
+        */
+
+        arrow.setEndPos(GetMousePosition());
+
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        {
+            float speed{Vector2Distance(arrow.getStartPos(),
+                                        arrow.getEndPos()) *
+                        5};
+
+            float angleInRadian{
+                Vector2LineAngle(arrow.getStartPos(),
+                                 arrow.getEndPos())};
+
+            // setting the initial velocity
+            // dividing the X and Y component of
+            // velocity using following formulas:
+            // X initial velocity = speed * cos(angleInRadian)
+            // because negative Y means upwards direction
+            // Y initial velocity = -speed * sin(angleInRadian)
+            velocity.x = speed * std::cos(angleInRadian);
+            velocity.y = -speed * std::sin(angleInRadian);
+
+            constexpr float gravityPower{100};
+            // APPLYING GRAVITY TO THE BANANA
+            acceleration.y = 9.8f * gravityPower;
+        }
+
+        constexpr float delta{1.0f / targetFPS};
+
+        velocity.x += acceleration.x * delta;
+        velocity.y += acceleration.y * delta;
+
+        banana.setX(banana.getX() + velocity.x * delta);
+        banana.setY(banana.getY() + velocity.y * delta);
+
+        for (Rectangle2D &building : buildings)
+        {
+            if (CheckCollisionCircleRec(banana.getPosition(),
+                                        banana.getRadius(),
+                                        building.getRectangle()))
+            {
+
+                if (toggleChance)
+                {
+                    arrow.setStartPos(lGorilla.getPosition());
+                    banana.setPosition(lGorilla.getPosition());
+                }
+                else
+                {
+                    arrow.setStartPos(rGorilla.getPosition());
+                    banana.setPosition(rGorilla.getPosition());
+                }
+
+                toggleChance = !toggleChance;
+
+                velocity = Vector2{};
+                acceleration = Vector2{};
+
+                building.setHeight(building.getHeight() - banana.getRadius());
+                building.setY(screenH - building.getHeight());
+            }
+        }
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
@@ -52,6 +138,9 @@ int main()
 
         DrawRectangleRec(lGorilla.getRectangle(), lGorilla.getTint());
         DrawRectangleRec(rGorilla.getRectangle(), rGorilla.getTint());
+
+        DrawLineV(arrow.getStartPos(), arrow.getEndPos(), arrow.getTint());
+        DrawCircleV(banana.getPosition(), banana.getRadius(), banana.getTint());
 
         DrawFPS(0, 0);
 
